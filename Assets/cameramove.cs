331e0267b7ecfe5fdd3127c262fe3bc95fa8ac66
Isabele Timotio <isabele.timotio@gmail.com>
2023-05-10 10:37:06 -0300

    atualização 10/05
    Mira acompanhando o mouse

---
+++ Assets/cameramove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramove : MonoBehaviour
+{
+    public bool travarMouse = true; //Controla se o cursor do mouse é exibido
+    public float sensibilidade = 2.0f; //Controla a sensibilidade do mouse
+
+    public float mouseX = 0.0f, mouseY = 0.0f; //Variáveis que controla a rotação do mouse
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            if (!travarMouse)
+            {
+                return;
+            }
+              Cursor.visible = false; //Oculta o cursor do mouse
+              Cursor.lockState = CursorLockMode.Locked; //Trava o cursor do centro
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      mouseX += Input.GetAxis("Mouse X") * sensibilidade; // Incrementa o valor do eixo X e multiplica pela sensibilidade
+      mouseY -= Input.GetAxis("Mouse Y") * sensibilidade; // Incrementa o valor do eixo Y e multiplica pela sensibilidade. (Obs. usamos o - para inverter os valores)
+
+      transform.eulerAngles = new Vector3(mouseY, mouseX, 0); //Executa a rotação da câmera de acordo com os eixos
+    }
+}
